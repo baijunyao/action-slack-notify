8b0aee721fe3341bb7256df515a050827acd7f62
baijunyao <baijunyao@baijunyao.com>
2019-10-20 21:48:42 +0800

    Add `attachments` to message

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,44 @@ import { WebClient, ChatPostMessageArguments } from '@slack/web-api'
 
 async function run() {
   try {
-    const status: string = core.getInput('status', { required: true });
+    const github_context = JSON.parse(core.getInput('github_context', { required: true }));
+    const job_context = JSON.parse(core.getInput('job_context', { required: true }));
     const channel: string = core.getInput('channel', { required: true });
     const slack_bot_token: string = core.getInput('slack_bot_token', { required: true });
     const web = new WebClient(slack_bot_token);
     let message: ChatPostMessageArguments;
+    let color: string;
+    let title: string;
 
-    if (status === 'success') {
-      message = {
-        channel: channel,
-        text: 'Success',
-      };
+    if (job_context.status === 'Success') {
+      title = 'GitHub Action Success';
+      color = '#008000';
     } else {
-      message = {
-        channel: channel,
-        text: 'Failure',
-      };
+      title = 'GitHub Action Failure';
+      color = '#FF0000';
     }
+
+    message = {
+      channel: channel,
+      text: '',
+      attachments: [
+        {
+          mrkdwn_in: ['fields'],
+          color: color,
+          author_name: github_context.event.head_commit.author.username,
+          author_link: 'https://github.com/' + github_context.event.head_commit.author.username,
+          title: title,
+          title_link: github_context.event.head_commit.url + '/checks',
+          fields: [
+            {
+              title: 'Commit',
+              value: '<' + github_context.event.head_commit.url + '|' + github_context.event.head_commit.message + '>',
+              short: false
+            }
+          ]
+        }
+      ]
+    };
 
     await web.chat.postMessage(message);
   } catch (err) {
