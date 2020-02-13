c61c589f77ef1d33d20ab845eeeeb743ee801c47
baijunyao <baijunyao@baijunyao.com>
2020-02-13 21:18:13 +0800

    Show branch and workflow name

--- src/main.ts
+++ src/main.ts
@@ -7,41 +7,51 @@ async function run() {
     const job_context = JSON.parse(core.getInput('job_context', { required: true }));
     const channel: string = core.getInput('channel', { required: true });
     const slack_bot_token: string = core.getInput('slack_bot_token', { required: true });
-    const web = new WebClient(slack_bot_token);
-    let message: ChatPostMessageArguments;
-    let color: string;
-    let title: string;
 
-    if (job_context.status === 'Success') {
-      title = 'GitHub Action Success';
-      color = '#008000';
-    } else {
-      title = 'GitHub Action Failure';
-      color = '#FF0000';
-    }
+    const branch: string = github_context.ref.slice(11);
+    const repositoryUrl = github_context.event.repository.url + "/";
+    const commitUrl = github_context.event.head_commit.url + "/";
 
-    message = {
+    const message: ChatPostMessageArguments = {
       channel: channel,
       text: '',
       attachments: [
         {
-          mrkdwn_in: ['fields'],
-          color: color,
-          author_name: github_context.event.head_commit.author.username,
-          author_link: 'https://github.com/' + github_context.event.head_commit.author.username,
-          title: title,
-          title_link: github_context.event.head_commit.url + '/checks',
-          fields: [
+          blocks: [
             {
-              title: 'Commit',
-              value: '<' + github_context.event.head_commit.url + '|' + github_context.event.head_commit.message + '>',
-              short: false
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*Status:* " + job_context.status
+              }
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*Commit:* <" + commitUrl + '|' + github_context.event.head_commit.message + ">"
+              }
+            },
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: "*Branch:*\n <" + repositoryUrl + "tree/" + branch + "|" + branch + ">"
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*Workflow:*\n <" + commitUrl + "checks|" + github_context.workflow + ">"
+                }
+              ]
             }
-          ]
+          ],
+          color: job_context.status === 'Success' ? '#008000' : '#FF0000'
         }
       ]
     };
 
+    const web = new WebClient(slack_bot_token);
     await web.chat.postMessage(message);
   } catch (err) {
     console.log(err)
